4ec30e643e57cae8fccb0e70cd2e347a56ee06e6
Shawn <zappeln@gmail.com>
2018-02-16 14:14:37 -0600

    created and populated flash cards for testing

---
+++ js/cards.js
@@ -0,0 +1,21 @@
+var cards = [
+  {name: "Nitrogen dioxide", formula: "NO<sub>2</sub>"},
+  {name: "Dichlorine heptoxide", formula: "Cl<sub>2</sub>O<sub>7</sub>"},
+  {name: "Calcium oxide", formula: "CaO"},
+  {name: "Osmium tetroxide", formula: "OsO<sub>4</sub>"},
+  {name: "Sulfuric acid", formula: "H<sub>2</sub>SO<sub>4</sub>"},
+  {name: "Nitrate", formula: "NO<sub>3</sub>-"},
+  {name: "Sodium Hydroxide", formula: "NaOH"},
+  {name: "Calcium Hydroxide", formula: "Ca(OH)<sub>2</sub>"},
+  {name: "Hypoclorite", formula: "ClO-"},
+  {name: "Calcium carbonate", formula: "CaCO<sub>3</sub>"},
+  {name: "Chlorine dioxide", formula: "ClO<sub>2</sub>"},
+  {name: "Calcium hypochlorite", formula: "Ca(ClO)<sub>2</sub>"},
+  {name: "Calcium sulfate", formula: "CaSO<sub>4</sub>"},
+  {name: "Nitric acid", formula: "HNO<sub>3</sub>"},
+  {name: "Sodium carbonate", formula: "Na<sub>2</sub>CO<sub>3</sub>"},
+  {name: "Magnesium sulfate", formula: "MgSO<sub>4</sub>"},
+  {name: "Potassium hydroxide", formula: "KOH"}
+]
+
+// {name: "", formula: ""}, <sub></sub>
