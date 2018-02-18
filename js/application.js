dca772b6e9bad2925514b233f39f4a6bb8f1aecd
Shawn <zappeln@gmail.com>
2018-02-17 22:38:33 -0600

    refactor code, add css

--- js/application.js
+++ js/application.js
@@ -7,10 +7,21 @@ function getCompoundArray(formula) {
   return formula.match(/[A-Z][a-z]?/g);
 }
 
+function answersMarkup(card, userInput) {
+  const markup = `
+    <div class="">
+      ${card.formula}<br>
+      ${card.name}<br>
+      ${userInput}<br>
+    </div>
+  `
+  return markup
+}
+
 function displayAnswer(userInput) {
   var color = 'tomato'
   if (card.name.toLowerCase() === userInput.toLowerCase()) {color = 'limegreen'};
   document.getElementById("answersField").style.backgroundColor = color;
-  document.getElementById("answersField").innerHTML = card.formula + " " + card.name + " " + userInput;
+  document.getElementById("answersField").innerHTML = answersMarkup(card, userInput);
 }
 
