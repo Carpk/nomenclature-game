52a2906bd1365862a76ba112bf144f91e3c136b7
Shawn <zappeln@gmail.com>
2018-02-20 18:08:24 -0600

    remove unneeded tags from html, setup for returing hints

--- js/application.js
+++ js/application.js
@@ -7,21 +7,39 @@ function getCompoundArray(formula) {
   return formula.match(/[A-Z][a-z]?/g);
 }
 
-function answersMarkup(card, userInput) {
+function answersMarkup(card, userInput, hint) {
   const markup = `
     <div class="">
       ${card.formula}<br>
       ${card.name}<br>
       ${userInput}<br>
+      <span id="hint-box">${hint}</span>
     </div>
   `
   return markup
 }
 
+function standardized(userInput) {
+  var input = userInput.trim().toLowerCase()
+
+  return input
+}
+
+function createHint(ans, input) {
+  hint = ""
+
+  return hint
+}
+
 function displayAnswer(userInput) {
-  var color = 'tomato'
-  if (card.name.toLowerCase() === userInput.trim().toLowerCase()) {color = 'limegreen'};
+  var color = 'limegreen'
+  var hint = ""
+  var input = userInput.trim().toLowerCase()
+  if (card.name.toLowerCase() !== input) {
+    color = 'tomato'
+    hint = createHint(card.name, input);
+  };
   document.getElementById("answersField").style.backgroundColor = color;
-  document.getElementById("answersField").innerHTML = answersMarkup(card, userInput);
+  document.getElementById("answersField").innerHTML = answersMarkup(card, userInput, hint);
 }
 
