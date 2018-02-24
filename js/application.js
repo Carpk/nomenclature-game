6bfd7b3b928b369a96efc4c2c9159f0b83de2b8b
Shawn <zappeln@gmail.com>
2018-02-23 22:19:22 -0600

    write markup for tips, css for tips box

--- js/application.js
+++ js/application.js
@@ -7,7 +7,7 @@ function getCompoundArray(formula) {
   return formula.match(/[A-Z][a-z]?/g);
 }
 
-function answersMarkup(card, userInput, hint) {
+function answersMarkup(card, userInput) {
   const markup = `
     <div class="">
       ${card.formula}<br>
@@ -25,18 +25,28 @@ function standardized(userInput) {
 }
 
 function oxyanionMarkup() {
-  var text = "-ate suffix is used on the most common oxyanion.<br>-ite suffix is used on the oxyanion with one less oxygen"
+  var text = "<div class='hint-box rnd-corners'>-ate is used on the most common oxyanion.<br>-ite is used on the oxyanion with one less oxygen</div>"
   var example = "Chlorine comes in hypochlorite (ClO), chlorite (ClO2), chlorate (ClO3) and perchlorate (ClO4) forms."
   return text
 }
 
+function sOMETHINGnMarkup() {
+  var text = "<div class='hint-box rnd-corners'>-ide is used when the substance is binary (two nonmetal elements)</div>"
+  return text
+}
+
+function thioMarkup() {
+  var text = "<div class='hint-box rnd-corners'>thio- means an oxygen has been replaced with a sulfur within the oxyanion.</div>"
+  return text
+}
 
 function createHint(ans, input) {
   hint = ""
-  if (true) {
-    hint = oxyanionMarkup();
-  }
-  return hint
+  if (ans == 'ite' && input == 'ate' || ans == 'ate' && input == 'ite') { hint = oxyanionMarkup(); }
+
+  if (ans == 'thio' && input != 'thio') { hint = thioMarkup(); }
+
+ return hint
 }
 
 function displayAnswer(userInput) {
@@ -46,8 +56,9 @@ function displayAnswer(userInput) {
   if (card.name.toLowerCase() !== input) {
     color = 'tomato'
     hint = createHint(card.name, input);
+    document.getElementById("hintsField").innerHTML = createHint(card.name, userInput);
   };
   document.getElementById("answerBox").style.backgroundColor = color;
-  document.getElementById("answersField").innerHTML = answersMarkup(card, userInput, hint);
+  document.getElementById("answersField").innerHTML = answersMarkup(card, userInput);
 }
 
